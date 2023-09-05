6c7eea813902da314b05a654256b70589fd545cb
yaojy3 <113939487+yaojy3@users.noreply.github.com>
2023-09-05 08:22:58 -0400

    Add files via upload

---
+++ my_package/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jesse Yao',
+    maintainer_email='yaojy@dukes.jmu.edu',
+    description='Beginner client lbraries tutorials practice package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = my_package.my_node:main'
+        ],
+    },
+)
